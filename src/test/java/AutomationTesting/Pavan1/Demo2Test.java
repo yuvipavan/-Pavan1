4656b11b19b3b2759acf9af57582791871362afc
yuvipavan <pavan.vandana1247@gmail.com>
2017-10-09 20:30:57 +0530

    Now Done

---
+++ src/test/java/AutomationTesting/Pavan1/Demo2Test.java
@@ -0,0 +1,16 @@
+package AutomationTesting.Pavan1;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+	
+	@Test
+	public void justTest1()
+	{
+		System.out.println("Hello Maven");
+		
+		
+	}
+
+
+}
